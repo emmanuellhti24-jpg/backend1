f8a0b63f567117807a772f15acbd198a32c588f4
emmanuellh.ti24@utsjr.edu.mx <emmanuellh.ti24@utsjr.edu.mx>
2025-10-07 02:33:45 -0600

    se agrego contacto

--- routes/rutas.js
+++ routes/rutas.js
@@ -1,18 +1,35 @@
-import {Router} from "express" //desestructuracion de {Routes}
+import {Router} from "express"
 
 const router = Router()
 
 var artistas = ["Johann Sebastian Bach", "Ludwig van Beethoven", "Wolfgang Amadeus Mozart", "Leonardo da Vinci", "Antonio Vivaldi"]
-router.get("/", (req, res)=>{
-    res.render("home", {artistas})
-})
 
+router.get("/", (req, res)=>{
+    res.render("home", {
+        artistas,
+        title: "Inicio - Artistas Clásicos",
+        page: "home"
+    })
+})
 
 router.get("/info/:c/:texto", (req, res)=>{
     var c = req.params.c
     var texto = req.params.texto
     console.log(c)
-    res.render("info",{c, texto})
+    res.render("info", {
+        c, 
+        texto,
+        title: "Información",
+        page: "info"
+    })
+})
+
+// NUEVA RUTA PARA CONTACTO
+router.get("/contacto", (req, res)=>{
+    res.render("contacto", {
+        title: "Contáctanos",
+        page: "contacto"
+    })
 })
 
 export default router
